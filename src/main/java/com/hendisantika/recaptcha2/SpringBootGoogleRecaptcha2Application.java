92cb2085acb71981ae40bda219a9ef31c060af8c
hendisantika <hendisantika@yahoo.co.id>
2023-12-11 05:15:56 +0700

    Create starter project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/recaptcha2/SpringBootGoogleRecaptcha2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.recaptcha2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleRecaptcha2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGoogleRecaptcha2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/recaptcha2/SpringBootGoogleRecaptcha2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.recaptcha2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleRecaptcha2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
