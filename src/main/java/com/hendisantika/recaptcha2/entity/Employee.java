83a43f737e0399c3e85c04e48faf87790998b0db
hendisantika <hendisantika@yahoo.co.id>
2023-12-11 05:23:42 +0700

    Add Employee Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/com/hendisantika/recaptcha2/entity/Employee.java
@@ -0,0 +1,37 @@
+package com.hendisantika.recaptcha2.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-google-recaptcha2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/11/23
+ * Time: 05:23
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate dateOfBirth;
+}
