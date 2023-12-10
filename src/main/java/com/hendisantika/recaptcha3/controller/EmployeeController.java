60ef04b58ba086d6c406ce2fac660685f63436be
hendisantika <hendisantika@yahoo.co.id>
2023-12-11 05:31:22 +0700

    Add showAll in EmployeeController🫸🌀✏️📗🐧🐳

--- src/main/java/com/hendisantika/recaptcha3/controller/EmployeeController.java
+++ src/main/java/com/hendisantika/recaptcha3/controller/EmployeeController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.recaptcha3.controller;
 
+import com.hendisantika.recaptcha3.entity.Employee;
 import com.hendisantika.recaptcha3.service.EmployeeService;
 import com.hendisantika.recaptcha3.service.RecaptchaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +27,15 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     private final RecaptchaService recaptchaService;
+
+    @GetMapping(path = {"/", "/all"})
+    public String showAll(Model model) {
+
+        List<Employee> employeeEntityList = employeeService.findAll();
+
+        model.addAttribute("employees", employeeEntityList);
+
+        return "index";
+    }
+
 }
